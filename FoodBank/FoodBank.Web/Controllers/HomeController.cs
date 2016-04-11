65cd76a02b4d2772c32bbbec70383d385a1f5c90
Mark OGrady <mark.ogrady@marketplacelab.com>
2016-04-11 13:48:39 +0100

    Removed Logo Url Because we will upload the image in the system. Also removed some icons and added address

--- FoodBank/FoodBank.Web/Controllers/HomeController.cs
+++ FoodBank/FoodBank.Web/Controllers/HomeController.cs
@@ -13,18 +13,13 @@ namespace FoodBank.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Terms()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+    
     }
 }
