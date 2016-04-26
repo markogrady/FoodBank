b1ffe7cc4dff2f268bf019fda57b81e8b3fb1205
leesole <lee_sole@yahoo.com>
2016-04-26 17:34:39 +0100

    Changes to company web controller to display basic info (was looking at wrong model)

--- FoodBank/FoodBank.Core/Dto/Company/CompanyIndexModel.cs
+++ FoodBank/FoodBank.Core/Dto/Company/CompanyIndexModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodBank.Core.Data.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,15 @@ namespace FoodBank.Core.Dto.Company
         public Guid CompanyId { get; set; }
         public string CompanyName { get; set; }
         public string LogoUrl { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string TownCity { get; set; }
+        public County County { get; set; }
+        public string PostCode { get; set; }
+
         public string ContactName { get; set; }
         public string ContactPhoneNumber { get; set; }
+        public string ContactEmailAddress { get; set; }
     }
 }
